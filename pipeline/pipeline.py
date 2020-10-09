ab03fa03b02ec95706d9b09f795d933455718117
sophiabj <63017339+sophiabj@users.noreply.github.com>
2020-10-09 08:39:14 +0200

    Update pipeline.py

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,7 +5,7 @@ def preprocess_op():
 
     return dsl.ContainerOp(
         name='Preprocess Data',
-        image='gnovack/boston_pipeline_preprocessing:latest',
+        image='gcr.io/kube01/kubeflow/presidentialelections/preprocess:latest',
         arguments=[],
         file_outputs={
             'x_train': '/app/x_train.npy',
@@ -19,7 +19,7 @@ def train_op(x_train, y_train):
 
     return dsl.ContainerOp(
         name='Train Model',
-        image='gnovack/boston_pipeline_train:latest',
+        image='gcr.io/kube01/kubeflow/presidentialelections/train:latest',
         arguments=[
             '--x_train', x_train,
             '--y_train', y_train
@@ -33,7 +33,7 @@ def test_op(x_test, y_test, model):
 
     return dsl.ContainerOp(
         name='Test Model',
-        image='gnovack/boston_pipeline_test:latest',
+        image='gcr.io/kube01/kubeflow/presidentialelections/test:latest',
         arguments=[
             '--x_test', x_test,
             '--y_test', y_test,
@@ -48,17 +48,17 @@ def deploy_model_op(model):
 
     return dsl.ContainerOp(
         name='Deploy Model',
-        image='gnovack/boston_pipeline_deploy_model:latest',
+        image='gcr.io/kube01/kubeflow/presidentialelections/deploy:latest',
         arguments=[
             '--model', model
         ]
     )
 
 @dsl.pipeline(
-   name='Boston Housing Pipeline',
+   name='Presidential Elections Pipeline',
    description='An example pipeline that trains and logs a regression model.'
 )
-def boston_pipeline():
+def presidential_elections_pipeline():
     _preprocess_op = preprocess_op()
     
     _train_op = train_op(
@@ -77,4 +77,4 @@ def boston_pipeline():
     ).after(_test_op)
 
 client = kfp.Client()
-client.create_run_from_pipeline_func(boston_pipeline, arguments={})
+client.create_run_from_pipeline_func(presidential_elections_pipeline, arguments={})
